525401fc3693b87ff0580a01d892a7c0773d13dd
Ayouub-aj <aigle313@gmail.com>
2024-03-20 23:35:48 +0000

    intro

---
+++ Intro_to_Programming/Arithmetics_Variables/question4.py
@@ -0,0 +1,8 @@
+#calculate how many births there is in a day... fyi births per min = 250
+
+hours_per_day   = 24
+min_per_hour    = 60
+births_per_min  = 250
+
+births_per_day  = hours_per_day * min_per_hour * births_per_min
+print(births_per_day)
