c2253a7f97c3ae87ea1283ad898f361cd1d55bf6
Ayouub-aj <aigle313@gmail.com>
2026-01-19 20:00:11 +0100

    question1

--- Intro_to_Programming/Data_types/question1.py
+++ Intro_to_Programming/Data_types/question1.py
@@ -9,3 +9,6 @@ print(type(y))
 z = int(y)
 print(z)
 print(type(z))
+
+# Negative floats are always rounded UP to the closest integer (for instance, both -1.1 and -1.9 are rounded up to -1). 
+# Positive floats are always rounded DOWN to the closest integer (for instance, 2.1 and 2.9 are rounded down to 2).
