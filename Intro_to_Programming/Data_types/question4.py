a452b9376b26d83b9a8d3a17a6b70186236d06a5
Ayouub-aj <aigle313@gmail.com>
2026-01-19 20:27:25 +0100

    question4

---
+++ Intro_to_Programming/Data_types/question4.py
@@ -0,0 +1,9 @@
+# We'll continue our study of boolean arithmetic. For this question, your task is to provide a description of what happpens when you add booleans.
+
+print(False + False)
+print(True + False)
+print(False + True)
+print(True + True)
+print(False + True + True + True)
+
+# When you add booleans, adding False is equivalent to adding 0, and adding True is equivalent to adding 1.
