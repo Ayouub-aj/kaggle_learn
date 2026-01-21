570cb724d411749bfdd88defb403815950485b58
Ayouub-aj <aigle313@gmail.com>
2026-01-21 03:23:25 +0100

    question1

--- Intro_to_Programming/Lists/question1.py
+++ Intro_to_Programming/Lists/question1.py
@@ -1,3 +1,12 @@
+# You own a restaurant with five food dishes, organized in the Python list menu below. One day, you decide to:
+
+# remove bean soup ('bean soup') from the menu, and
+# add roasted beet salad ('roasted beet salad') to the menu.
+# Implement this change to the list below. While completing this task,
+
+# do not change the line that creates the menu list.
+# your answer should use .remove() and .append().
+
 # Do not change: Initial menu for your restaurant
 menu = ['stewed meat with onions', 'bean soup', 'risotto with trout and shrimp',
        'fish soup with cream and onion', 'gyro']
